f7e4f497388e6bc808c3d6e6124ef50c238d6c48
kyungil9 <nittoo@naver.com>
2024-01-12 18:05:39 +0900

    feat : misson detail 화면 임시 추가 - misson detail 화면 초기 생성

---
+++ feature/mission/src/main/kotlin/jinproject/stepwalk/mission/screen/missondetail/MissonDetailViewModel.kt
@@ -0,0 +1,12 @@
+package jinproject.stepwalk.mission.screen.missondetail
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+internal class MissonDetailViewModel @Inject constructor(
+
+) : ViewModel(){
+
+}
