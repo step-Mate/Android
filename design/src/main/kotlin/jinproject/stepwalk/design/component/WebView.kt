f8f6091979f22ec320a2b1cd214339d9a91c0e2d
kyungil9 <nittoo@naver.com>
2024-03-11 14:51:22 +0900

    fix : 이용약관 부분 이동 - 이용약관 화면을 design으로 이동

--- design/src/main/kotlin/jinproject/stepwalk/design/component/WebView.kt
+++ design/src/main/kotlin/jinproject/stepwalk/design/component/WebView.kt
@@ -2,8 +2,18 @@ package jinproject.stepwalk.design.component
 
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
+import jinproject.stepwalk.design.R
 
 @Composable
 fun WebView(
@@ -21,4 +31,27 @@ fun WebView(
         }
     )
 }
+
+@Composable
+fun TermsScreen(
+    popBackStack: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+    ) {
+        StepMateBoxDefaultTopBar(
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.surface)
+                .windowInsetsPadding(WindowInsets.statusBars),
+            icon = R.drawable.ic_arrow_left_small,
+            onClick = popBackStack
+        ) {
+            HeadlineText(text = "이용 약관", modifier = Modifier.align(Alignment.Center))
+        }
+        WebView(url = "https://sites.google.com/view/stepmate-personal-info-policy/%ED%99%88")
+    }
+}
+
 
