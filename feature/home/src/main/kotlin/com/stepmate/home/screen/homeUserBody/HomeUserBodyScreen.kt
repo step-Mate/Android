542368a9d27c88ea52c1d81878150b288fead5eb
jinho <whangwan@gmail.com>
2024-03-12 23:09:33 +0900

    fix : 신체정보 입력 화면 수정
    - NumberPicker 형식이 UX 가 너무 안좋아서 (한참내리고 올려야함) TextField 로 변경

--- feature/home/src/main/kotlin/com/stepmate/home/screen/homeUserBody/HomeUserBodyScreen.kt
+++ feature/home/src/main/kotlin/com/stepmate/home/screen/homeUserBody/HomeUserBodyScreen.kt
@@ -1,133 +1,95 @@
 package com.stepmate.home.screen.homeUserBody
 
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavOptions
 import androidx.navigation.navOptions
-import com.stepmate.design.appendColorText
-import com.stepmate.design.component.DescriptionAnnotatedLargeText
+import com.stepmate.core.SnackBarMessage
+import com.stepmate.design.component.DefaultOutlinedTextField
 import com.stepmate.design.component.DescriptionLargeText
 import com.stepmate.design.component.DescriptionSmallText
-import com.stepmate.design.component.HorizontalWeightSpacer
 import com.stepmate.design.component.StepMateBoxDefaultTopBar
-import com.stepmate.design.component.StepMateNumberPicker
 import com.stepmate.design.component.VerticalSpacer
-import com.stepmate.design.component.clickableAvoidingDuplication
 import com.stepmate.design.component.layout.DefaultLayout
 import com.stepmate.design.theme.StepMateTheme
 import com.stepmate.home.navigation.homeUserBody
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
 
 @Composable
 internal fun HomeUserBodyScreen(
     homeUserBodyViewModel: HomeUserBodyViewModel = hiltViewModel(),
     navigateToHome: (NavOptions?) -> Unit,
+    showSnackBar: (SnackBarMessage) -> Unit,
 ) {
     val age by homeUserBodyViewModel.age.collectAsStateWithLifecycle()
     val weight by homeUserBodyViewModel.weight.collectAsStateWithLifecycle()
     val height by homeUserBodyViewModel.height.collectAsStateWithLifecycle()
-    val bottomSheetType by homeUserBodyViewModel.bottomSheetType.collectAsStateWithLifecycle()
 
     HomeUserBodyScreen(
         age = age,
         weight = weight,
         height = height,
-        bottomSheetType = bottomSheetType,
         updateAge = homeUserBodyViewModel::updateAge,
         updateWeight = homeUserBodyViewModel::updateWeight,
         updateHeight = homeUserBodyViewModel::updateHeight,
-        updateSheetType = homeUserBodyViewModel::updateBottomSheetType,
         navigateToHome = navigateToHome,
+        showSnackBar = showSnackBar,
     )
 }
 
-@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
 private fun HomeUserBodyScreen(
-    coroutineScope: CoroutineScope = rememberCoroutineScope(),
     age: Int,
     weight: Int,
     height: Int,
-    bottomSheetType: HomeUserBodyViewModel.BottomSheetType,
     updateAge: (Int) -> Unit,
     updateWeight: (Int) -> Unit,
     updateHeight: (Int) -> Unit,
-    updateSheetType: (HomeUserBodyViewModel.BottomSheetType) -> Unit,
     navigateToHome: (NavOptions?) -> Unit,
+    showSnackBar: (SnackBarMessage) -> Unit,
 ) {
-    val sheetState = rememberModalBottomSheetState()
-
-    val pagerItems by rememberUpdatedState(
-        newValue = when (bottomSheetType) {
-            HomeUserBodyViewModel.BottomSheetType.Age -> (12..100).toList()
-            HomeUserBodyViewModel.BottomSheetType.Weight -> (20..200).toList()
-            HomeUserBodyViewModel.BottomSheetType.Height -> (100..250).toList()
-        }
-    )
-
-    val pagerState = rememberPagerState(
-        when (bottomSheetType) {
-            HomeUserBodyViewModel.BottomSheetType.Age -> 8
-            HomeUserBodyViewModel.BottomSheetType.Weight -> 50
-            HomeUserBodyViewModel.BottomSheetType.Height -> 70
-        }
-    ) {
-        pagerItems.size
-    }
-
-    var isSheetVisible by remember {
-        mutableStateOf(false)
-    }
-
-    StepMateNumberPicker(
-        isSheetVisible = isSheetVisible,
-        items = pagerItems,
-        pagerState = pagerState,
-        sheetState = sheetState,
-        updateIsSheetVisibility = { bool -> isSheetVisible = bool }
-    ) {
-        val value = pagerItems[pagerState.currentPage]
-
-        when (bottomSheetType) {
-            HomeUserBodyViewModel.BottomSheetType.Age -> updateAge(value)
-            HomeUserBodyViewModel.BottomSheetType.Weight -> updateWeight(value)
-            HomeUserBodyViewModel.BottomSheetType.Height -> updateHeight(value)
-        }
-    }
+    val focusManager = LocalFocusManager.current
 
     DefaultLayout(
+        modifier = Modifier
+            .fillMaxSize()
+            .imePadding()
+            .verticalScroll(rememberScrollState())
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null,
+                onClick = {
+                    focusManager.clearFocus()
+                }
+            ),
         topBar = {
             StepMateBoxDefaultTopBar(
                 modifier = Modifier
@@ -137,11 +99,19 @@ private fun HomeUserBodyScreen(
                 icon = com.stepmate.design.R.drawable.ic_arrow_right_small,
                 iconAlignment = Alignment.CenterEnd,
                 onClick = {
-                    navigateToHome(navOptions {
-                        popUpTo(homeUserBody) {
-                            inclusive = true
-                        }
-                    })
+                    if (age == 0 || weight == 0 || height == 0)
+                        showSnackBar(
+                            SnackBarMessage(
+                                headerMessage = "신체정보를 입력해 주세요.",
+                                contentMessage = "신체정보가 없으면 정확한 칼로리를 계산할 수 없어요."
+                            )
+                        )
+                    else
+                        navigateToHome(navOptions {
+                            popUpTo(homeUserBody) {
+                                inclusive = true
+                            }
+                        })
                 },
             )
         }
@@ -151,90 +121,69 @@ private fun HomeUserBodyScreen(
         DescriptionSmallText(text = "서비스 이용에 앞서 정확한 칼로리 소모량 계산을 위해 신체정보를 수집하고 있어요.")
         VerticalSpacer(height = 4.dp)
         DescriptionSmallText(text = "신체 정보는 회원가입을 하지 않은 경우 회원님의 로컬 경로에 저장되며, StepMate는 이를 절대로 수집하지 않으니 걱정하지 않으셔도 되요.")
+        VerticalSpacer(height = 4.dp)
+        DescriptionSmallText(text = "회원 가입을 할 경우 회원님의 정보를 저장하기 위해 전송되지만, 비밀번호 및 신체정보는 모두 암호화 하여 저장하고 있어요.")
         VerticalSpacer(height = 50.dp)
 
-        BodyInfo(
-            value = age,
-            bottomSheetType = HomeUserBodyViewModel.BottomSheetType.Age,
-            onClick = {
-                coroutineScope.launch {
-                    updateSheetType(HomeUserBodyViewModel.BottomSheetType.Age)
-                    isSheetVisible = true
-                    sheetState.expand()
-                }
-            },
-        )
-        VerticalSpacer(height = 30.dp)
-        BodyInfo(
-            value = weight,
-            bottomSheetType = HomeUserBodyViewModel.BottomSheetType.Weight,
-            onClick = {
-                coroutineScope.launch {
-                    updateSheetType(HomeUserBodyViewModel.BottomSheetType.Weight)
-                    isSheetVisible = true
-                    sheetState.expand()
-                }
-            },
-        )
-        VerticalSpacer(height = 30.dp)
-        BodyInfo(
-            value = height,
-            bottomSheetType = HomeUserBodyViewModel.BottomSheetType.Height,
-            onClick = {
-                coroutineScope.launch {
-                    updateSheetType(HomeUserBodyViewModel.BottomSheetType.Height)
-                    isSheetVisible = true
-                    sheetState.expand()
-                }
-            },
-        )
-
+        Column(
+            modifier = Modifier
+                .background(
+                    MaterialTheme.colorScheme.surface,
+                    RoundedCornerShape(20.dp)
+                )
+                .padding(horizontal = 16.dp, vertical = 12.dp),
+        ) {
+            BodyInfo(
+                hint = "나이 입력하기",
+                text = age.toString(),
+                updateValue = updateAge,
+            )
+            VerticalSpacer(height = 30.dp)
+            BodyInfo(
+                hint = "몸무게 입력하기",
+                text = weight.toString(),
+                updateValue = updateWeight,
+            )
+            VerticalSpacer(height = 30.dp)
+            BodyInfo(
+                hint = "키 입력하기",
+                text = height.toString(),
+                updateValue = updateHeight,
+            )
+        }
     }
 }
 
 @Composable
 internal fun BodyInfo(
-    value: Int,
-    bottomSheetType: HomeUserBodyViewModel.BottomSheetType,
-    onClick: () -> Unit,
+    hint: String,
+    text: String,
+    updateValue: (Int) -> Unit,
 ) {
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 20.dp)
-            .background(MaterialTheme.colorScheme.surface, RoundedCornerShape(20.dp))
-            .border(1.dp, MaterialTheme.colorScheme.outline, RoundedCornerShape(20.dp))
-            .padding(horizontal = 12.dp, vertical = 10.dp)
-            .clickableAvoidingDuplication {
-                onClick()
-            },
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        val infoText = buildAnnotatedString {
-            if (value == 0)
-                append("${bottomSheetType.display} 입력하기")
-            else {
-                append("$value ")
-                appendColorText(
-                    text = when (bottomSheetType) {
-                        HomeUserBodyViewModel.BottomSheetType.Age -> "세"
-                        HomeUserBodyViewModel.BottomSheetType.Weight -> "kg"
-                        HomeUserBodyViewModel.BottomSheetType.Height -> "cm"
-                    }, color = MaterialTheme.colorScheme.onSurfaceVariant
-                )
-            }
-        }
-
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            DescriptionAnnotatedLargeText(text = infoText)
-            HorizontalWeightSpacer(float = 1f)
-            Icon(
-                painter = painterResource(id = com.stepmate.design.R.drawable.ic_arrow_right_small),
-                contentDescription = null,
-                tint = MaterialTheme.colorScheme.onSurfaceVariant,
-            )
+    var textState by remember {
+        mutableStateOf("").apply {
+            if (text != "0")
+                value = text
         }
     }
+
+    DefaultOutlinedTextField(
+        modifier = Modifier,
+        informationText = hint,
+        value = textState,
+        onNewValue = { value ->
+            textState = value
+            if (value.isNotEmpty())
+                updateValue(value.toInt())
+        },
+        keyboardType = KeyboardType.Decimal,
+        trailingIcon = {
+            Icon(
+                painter = painterResource(id = com.stepmate.design.R.drawable.ic_arrow_right_small),
+                contentDescription = null
+            )
+        }
+    )
 }
 
 @Composable
@@ -244,11 +193,10 @@ private fun PreviewHomeUserBodyScreen() = StepMateTheme {
         age = 0,
         weight = 60,
         height = 0,
-        bottomSheetType = HomeUserBodyViewModel.BottomSheetType.Age,
         updateAge = {},
         updateWeight = {},
         updateHeight = {},
-        updateSheetType = {},
         navigateToHome = {},
+        showSnackBar = {},
     )
 }
