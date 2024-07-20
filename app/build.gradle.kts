4c43a03f25b7cf78c1d33cefcd9f067194320d28
jino <whangwan@gmail.com>
2024-07-20 15:14:08 +0900

    Release : 1.0.2
    - Build Logic 수정
    - 걸음수 수집 로직 수정 및 단위테스트 추가
    - Kotlinx.collections.immutable 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
     defaultConfig {
         applicationId = "com.stepmate.app"
         targetSdk = 34
-        versionCode = 8
-        versionName = "1.0.1"
+        versionCode = 9
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
